2aa7513717998f6e6f7e9b589081d212bbdd88c3
Lasse Sofus Moller Sorensen <lassesofus@gmail.com>
2025-04-05 20:27:39 +0200

    Did ex. 5

---
+++ 1_4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BSUB -J profile_jacobi
+#BSUB -q hpc
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -W 1:00
+#BSUB -o output_%J.log
+#BSUB -e error_%J.err
+#BSUB -R "select[model == XeonGold6226R]"
+
+lscpu
+
+# Initialize Python environment
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+kernprof -l -v simulate.py 12
