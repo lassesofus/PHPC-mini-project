dafe30b7223f67d5a9e648bb7b0287c877fdc932
SentientSchnitzel <simondanielschneider@gmail.com>
2025-05-04 09:42:35 +0200

    remove actvenv

--- activate_conda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script is used to activate a conda environment
-source /dtu/projects/02613_2025/conda/conda_init.sh
-conda activate 02613
-
-# Interpreter
-# /dtu/projects/02613_2025/conda/miniconda3/envs/02613/bin/python
